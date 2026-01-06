737e7e11010a33fef84cdd440ff977ccee766edc
Jendri Dariel Montero D Oleo <jdari.mp4@gmail.com>
2026-01-06 00:15:59 -0400

    Add Spam.lua for party creation functionality

---
+++ Source/Spam.lua
@@ -0,0 +1,25 @@
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local RunService = game:GetService("RunService")
+local Services = ReplicatedStorage
+    :WaitForChild("Shared")
+    :WaitForChild("Packages")
+    :WaitForChild("Knit")
+    :WaitForChild("Services")
+local PartyRF = Services:WaitForChild("PartyService"):WaitForChild("RF")
+local ProximityRF = Services:WaitForChild("ProximityService"):WaitForChild("RF")
+local start = os.clock()
+local duration = 10
+local conn
+conn = RunService.RenderStepped:Connect(function()
+    if os.clock() - start >= duration then
+        conn:Disconnect()
+        return
+    end
+    PartyRF.CreateParty:InvokeServer("Golem")
+    ProximityRF.Functionals:InvokeServer(
+        Workspace.Proximity.CreateParty
+    )
+    PartyRF.Activate:InvokeServer()
+end)
